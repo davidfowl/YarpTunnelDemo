bbdddd1c945e1ca034fbebbee450f57b59897736
David Fowler <davidfowl@gmail.com>
2022-05-04 23:23:36 -0700

    Removed extra space

--- Backend/Program.cs
+++ Backend/Program.cs
@@ -3,7 +3,6 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddReverseProxy()
        .LoadFromConfig(builder.Configuration.GetSection("ReverseProxy"));
 
-
 // This is the HTTP/2 endpoint to register this app as part of the cluster endpoint
 var url = builder.Configuration["Tunnel:Url"]!;
 
