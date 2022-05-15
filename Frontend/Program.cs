410a57efe30520d053dd724f965045cf047db7c8
David Fowler <davidfowl@gmail.com>
2022-05-15 00:43:31 -0700

    Added comments for websocket connections

--- Frontend/Program.cs
+++ Frontend/Program.cs
@@ -7,9 +7,13 @@ builder.Services.AddTunnelServices();
 
 var app = builder.Build();
 
-app.UseWebSockets();
+// Uncomment to support websocket connections
+// app.UseWebSockets();
 
 app.MapReverseProxy();
+
+// Uncomment to support websocket connections
+// app.MapWebSocketTunnel("/connect-ws");
 
 // Auth can be added to this endpoint and we can restrict it to certain points
 // to avoid exteranl traffic hitting it
